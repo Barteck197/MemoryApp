55ab23b906e6b5baa05294cca3b850a42882486b
Grzegorz-linux <gfbardski@outlook.com>
2020-06-06 23:27:52 +0200

    implementing runLater idea. Now no errors. App still not working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,19 +6,45 @@ import javafx.scene.control.Label;
 
 public class Controller {
 
+    static int seconds = 0;
+    static int minutes = 0;
+    static int hours = 0;
+
     static boolean state = true;
 
 
     @FXML
     Label appTime;
 
-    TimeFlow timeFlow = new TimeFlow(appTime);
-
     @FXML
     private void startTime() {
         state = true;
 
-        Platform.runLater(timeFlow);
+        Platform.runLater(() -> {
+            while (state) {
+                try {
+                    Thread.sleep(1000);
+
+                    if (seconds >= 60) {
+                        seconds = 0;
+                        minutes++;
+                    }
+                    if (minutes >= 60) {
+                        seconds = 0;
+                        minutes = 0;
+                        hours++;
+                    }
+
+                    seconds++;
+
+                    appTime.setText(minutes + " : " + seconds);
+                    System.out.println(hours + " : " + minutes + " : " + seconds);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
        /*Thread t = new Thread() {
             @Override
