532a2db2066f8c5c6caa52610c49072c20c16286
Barteck197 <64739510+Barteck197@users.noreply.github.com>
2020-06-06 14:02:23 +0200

    update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,9 @@ module MemoryApp {
     requires javafx.controls;
     requires javafx.base;
     requires javafx.graphics;
+    requires javafx.web;
+    requires javafx.media;
+    requires javafx.swing;
 
     opens sample;
 }
