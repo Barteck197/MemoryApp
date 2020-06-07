5d2448b44b3074037efcba2e512a6c74f6357d6d
Barteck197 <64739510+Barteck197@users.noreply.github.com>
2020-06-07 18:51:32 +0200

    adding another scene

--- src/MainMenu/Controller.java
+++ src/MainMenu/Controller.java
@@ -1,7 +1,15 @@
 package MainMenu;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class Controller {
     static int seconds = 0;
@@ -46,4 +54,17 @@ public class Controller {
         };
         t.start();
     }
+
+    @FXML
+    private void addScene(javafx.event.ActionEvent event) throws IOException {
+
+        Parent view = FXMLLoader.load(getClass().getResource("board.fxml"));
+
+        Scene scene2 = new Scene(view, Color.BLUE);
+
+        Stage window = new Stage();
+        window.setScene(scene2);
+        window.show();
+    }
+
 }
--- src/plansza/menuController.java
+++
@@ -1,4 +0,0 @@
-package plansza;
-
-public class menuController {
-}
