f46d53f2d489a1b32e352fe9f12e5a3f022cf288
Barteck197 <64739510+Barteck197@users.noreply.github.com>
2020-06-07 20:11:16 +0200

    fixing window width

--- src/game/Main.java
+++ src/game/Main.java
@@ -14,9 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Memory Game");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
-
     }
-
 
     public static void main(String[] args) {
         launch(args);
