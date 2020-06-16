f47c8829ffed3d35ed6384e071629fcf4acb3b12
Barteck197 <64739510+Barteck197@users.noreply.github.com>
2020-06-16 10:22:34 +0200

    creating class player

---
+++ src/game/Player.java
@@ -0,0 +1,30 @@
+package game;
+
+public class Player {
+    private String name;
+    private int playerResult;
+    static  int id;
+
+    public Player(String name, int playerResult) {
+        this.name = name;
+        this.playerResult = playerResult;
+        id = 1;
+        id++;
+    }
+
+    //getters
+    public int getPlayerResult() {
+        return playerResult;
+    }
+    public String getName() {
+        return name;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPlayerResult(int playerResult) {
+        this.playerResult = playerResult;
+    }
+}
--- src/game/ScoreSaverController.java
+++ src/game/ScoreSaverController.java
@@ -1,5 +1,20 @@
 package game;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ScoreSaverController {
 
+    @FXML
+    Button saveResult;
+
+    @FXML
+    TextField playerName;
+
+
+    //TODO communication with object - player
+    public void saveResult() {
+        new Player(playerName.getText(), 20);
+    }
 }
