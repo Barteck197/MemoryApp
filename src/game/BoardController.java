6e4b97579dbf292d07809c093578174a5474b3d2
Barteck197 <64739510+Barteck197@users.noreply.github.com>
2020-06-16 18:53:34 +0200

    another attempt to initialize board. Still not working

--- src/game/BoardController.java
+++ src/game/BoardController.java
@@ -2,22 +2,18 @@ package game;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class BoardController {
 
