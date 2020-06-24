8e2abb53e92b1c591210808e538b97f288b9a6fb
Grzegorz-linux <gfbardski@outlook.com>
2020-06-24 22:21:34 +0200

    saving player to file

--- src/game/ScoreSaverController.java
+++ src/game/ScoreSaverController.java
@@ -5,7 +5,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class ScoreSaverController implements Exitable{
 
@@ -18,8 +21,15 @@ public class ScoreSaverController implements Exitable{
 
 
     //TODO communication with object - player
-    public void saveResult() {
-        new Player(playerName.getText(), 20);
+    public void saveResult() throws IOException {
+        Player pl = new Player(playerName.getText(), 20);
+
+        FileOutputStream fs = new FileOutputStream("highScores.txt");
+        ObjectOutputStream os = new ObjectOutputStream(fs);
+
+        os.writeObject(pl);
+        os.flush();
+        os.close();
     }
 
     @Override
